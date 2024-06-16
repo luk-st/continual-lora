fa030834828444ed3c432fc8bccdb38cfd008cb6
kzaleskaa <kzaleska416@gmail.com>
2024-06-16 13:17:40 +0200

    update metrics to work with multiple predictions

--- lora/metrics.py
+++ lora/metrics.py
@@ -1,3 +1,5 @@
+import glob
+import os
 from typing import List
 
 import torch
@@ -11,42 +13,32 @@ DINO_MODEL_NAME = "facebook/dino-vits16"
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def clip_image_metric(pred_img_path: str, ref_img_paths: List[str]) -> float:
+def collect_jpg_files(directory: str) -> List[str]:
+    jpg_files = glob.glob(os.path.join(directory, "**", "*.jpg"), recursive=True)
+    return jpg_files
+
+
+def clip_image_metric(pred_imgs: List[Image.Image], ref_path: str) -> float:
     clip_processor = AutoProcessor.from_pretrained(CLIP_MODEL_NAME)
     clip_model = AutoModel.from_pretrained(CLIP_MODEL_NAME).to(DEVICE)
 
-    pred_img = Image.open(pred_img_path)
-    ref_imgs = [Image.open(img) for img in ref_img_paths]
+    pred_img_processed = clip_processor(images=pred_imgs, return_tensors="pt").pixel_values.to(DEVICE)
+    pred_features = clip_model.get_image_features(pred_img_processed).unsqueeze(1)  # shape: [n_pred, 1, feature_dim]
 
-    pred_img = clip_processor(images=pred_img, return_tensors="pt").to(DEVICE)
-    pred_features = clip_model.get_image_features(**pred_img)
+    ref_imgs = collect_jpg_files(ref_path)
+    ref_imgs = [Image.open(img) for img in ref_imgs]
+    ref_imgs_processed = clip_processor(images=ref_imgs, return_tensors="pt").pixel_values.to(DEVICE)
+    ref_features = clip_model.get_image_features(ref_imgs_processed).unsqueeze(0)  # shape: [1, n_ref, feature_dim]
 
-    ref_imgs = clip_processor(images=ref_imgs, return_tensors="pt").to(DEVICE)
-    ref_features = clip_model.get_image_features(**ref_imgs)
+    cosine_similarities = F.cosine_similarity(pred_features, ref_features, dim=-1)  # shape: [n_pred, n_ref]
 
-    sim = F.cosine_similarity(pred_features, ref_features, dim=-1)
-    return sim.mean().item()
+    mean_similarities = cosine_similarities.mean(dim=1)  # shape: [n_pred]
+    overall_mean_similarity = mean_similarities.mean().item()
 
-
-def clip_text_metric(pred_img_path: str, ref_texts: List[str]) -> float:
-    clip_processor = AutoProcessor.from_pretrained(CLIP_MODEL_NAME)
-    clip_tokenizer = AutoTokenizer.from_pretrained(CLIP_MODEL_NAME)
-    clip_model = AutoModel.from_pretrained(CLIP_MODEL_NAME).to(DEVICE)
-
-    pred_img = Image.open(pred_img_path)
-    pred_img = clip_processor(images=pred_img, return_tensors="pt").to(DEVICE)
-    pred_features = clip_model.get_image_features(**pred_img)
-
-    ref_texts = clip_tokenizer(
-        ref_texts, return_tensors="pt", padding=True, truncation=True
-    ).to(DEVICE)
-    ref_features = clip_model.get_text_features(**ref_texts)
-
-    sim = F.cosine_similarity(pred_features, ref_features, dim=-1)
-    return sim.mean().item()
+    return overall_mean_similarity
 
 
-def dino_metric(pred_img_path: str, ref_img_paths: List[str]) -> float:
+def dino_metric(pred_imgs: List[Image.Image], ref_path: str) -> float:
     dino_model = AutoModel.from_pretrained(DINO_MODEL_NAME, add_pooling_layer=False).to(DEVICE)
 
     T = transforms.Compose(
@@ -58,12 +50,42 @@ def dino_metric(pred_img_path: str, ref_img_paths: List[str]) -> float:
         ]
     )
 
-    pred_img = T(Image.open(pred_img_path)).unsqueeze(0).to(DEVICE)
-    pred_features = dino_model(pred_img).last_hidden_state
+    pred_imgs_processed = torch.stack([T(img).to(DEVICE) for img in pred_imgs])
+    pred_features = dino_model(pred_imgs_processed).last_hidden_state[:, 0, :]  # shape: [n_pred, feature_dim]
 
-    ref_imgs = [Image.open(img) for img in ref_img_paths]
-    ref_imgs = torch.stack([T(img) for img in ref_imgs]).to(DEVICE)
-    ref_features = dino_model(ref_imgs).last_hidden_state
+    ref_imgs = collect_jpg_files(ref_path)
+    ref_imgs_processed = torch.stack([T(Image.open(img)).to(DEVICE) for img in ref_imgs])
+    ref_features = dino_model(ref_imgs_processed).last_hidden_state[:, 0, :]  # shape: [n_ref, feature_dim]
 
-    sim = F.cosine_similarity(pred_features[:, 0], ref_features[:, 0], dim=-1)
-    return sim.mean().item()
+    pred_features = pred_features.unsqueeze(1)  # shape: [n_pred, 1, feature_dim]
+    ref_features = ref_features.unsqueeze(0)  # shape: [1, n_ref, feature_dim]
+
+    cosine_similarities = F.cosine_similarity(pred_features, ref_features, dim=-1)
+    mean_similarities = cosine_similarities.mean(dim=1)  # shape: [n_pred]
+
+    overall_mean_similarity = mean_similarities.mean().item()
+
+    return overall_mean_similarity
+
+
+def clip_text_metric(pred_imgs: List[Image.Image], ref_texts: List[str]) -> float:
+    clip_processor = AutoProcessor.from_pretrained(CLIP_MODEL_NAME)
+    clip_tokenizer = AutoTokenizer.from_pretrained(CLIP_MODEL_NAME)
+    clip_model = AutoModel.from_pretrained(CLIP_MODEL_NAME).to(DEVICE)
+
+    processed_imgs = clip_processor(images=pred_imgs, return_tensors="pt").pixel_values.to(DEVICE)
+    pred_features = clip_model.get_image_features(processed_imgs)  # shape: [n_pred, feature_dim]
+
+    processed_texts = clip_tokenizer(text=ref_texts, return_tensors="pt", padding=True, truncation=True).input_ids.to(
+        DEVICE
+    )
+    ref_features = clip_model.get_text_features(processed_texts)  # shape: [n_ref, feature_dim]
+
+    pred_features = pred_features.unsqueeze(1)  # shape: [n_pred, 1, feature_dim]
+    ref_features = ref_features.unsqueeze(0)  # shape: [1, n_ref, feature_dim]
+    sim = F.cosine_similarity(pred_features, ref_features, dim=-1)  # shape: [n_pred, n_ref]
+
+    mean_sim = sim.mean(dim=1)  # shape: [n_pred]
+    overall_mean_sim = mean_sim.mean().item()
+
+    return overall_mean_sim
