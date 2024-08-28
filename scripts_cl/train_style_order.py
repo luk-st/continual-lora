1b9207ddc97b04b66c6630538751657cfc246b9a
kzaleskaa <kzaleska416@gmail.com>
2024-08-29 01:28:23 +0200

    add train style order script

--- scripts_cl/train_style_order.py
+++ scripts_cl/train_style_order.py
@@ -3,13 +3,16 @@ import os
 import random
 import subprocess
 
-DATASET_PATH = "./data/unlearn-canvas-dataset"
+DATASET_PATH = "./data/temp/combine"
 DATASET_SCP_PATH = "./data/train_unlearn"
-PROMPT_TEMPLATE = "image of {} in the style of {}"
+PROMPT_TEMPLATE = "{} image of {}"
+STYLES_TOKENS = ["skn style", "zwz style", "fci style", "rcf style", "cts style"]
 
 
-def get_work_dir(object_seed, order_seed):
-    return f"./models/seed_{object_seed}_style/seed_{order_seed}_order"
+def get_work_dir(style_seed, order_seed):
+    return (
+        f"./models/seed_{style_seed}_first_style_names_unlearn/seed_{order_seed}_order"
+    )
 
 
 def shuffle_lists(seed, lists):
@@ -25,35 +28,50 @@ def save_serialized_lists(path, serialized_lists):
         f.write("\n".join(serialized_lists))
 
 
-def generate_train_dataset():
+def generate_train_dataset(style_arg):
     random.seed(42)  # set seed to be ALWAYS the same
-    dirs = os.listdir(DATASET_PATH)
-    random_train_styles = random.choices(dirs, k=5)
+    dirs = sorted(os.listdir(DATASET_PATH))  # sort to ensure consistent order
+    dirs.remove("Seed_Images")  # remove seed images
+    random_train_styles = random.sample(sorted(dirs), k=5)
     results = [[], [], []]
 
     for idx, style in enumerate(random_train_styles):
-        objects = os.listdir(os.path.join(DATASET_PATH, style))
-        train_object, val_object = random.choices(objects, k=2)
-        imgs = os.listdir(os.path.join(DATASET_PATH, style, train_object))
-        chosen_img = random.choices(imgs, k=5)
+        objects = sorted(os.listdir(os.path.join(DATASET_PATH, style)))
+        train_object, val_object = random.sample(sorted(objects), k=2)
+        imgs = sorted(os.listdir(os.path.join(DATASET_PATH, style, train_object)))
+
+        print(os.path.join(DATASET_PATH, style, train_object))
+        chosen_img = random.sample(sorted(list(set(imgs))), k=5)
+        print(chosen_img)
 
         for img in chosen_img:
+            path_to_save = os.path.join(DATASET_SCP_PATH, style, train_object)
             os.makedirs(
-                os.path.join(DATASET_SCP_PATH, style, train_object),
+                path_to_save,
                 exist_ok=True,
             )
             os.system(
-                f"scp {os.path.join(DATASET_PATH, style, train_object, img)} {os.path.join(DATASET_SCP_PATH, style, train_object)}"
+                f"scp {os.path.join(DATASET_PATH, style, train_object, img)} {path_to_save}"
             )
 
         results[0].append(
             PROMPT_TEMPLATE.format(
-                train_object.replace("_", " ").lower(), style.replace("_", " ").lower()
+                (
+                    style.replace("_", " ").lower()
+                    if style_arg == "name"
+                    else STYLES_TOKENS[idx]
+                ),
+                train_object.replace("_", " ").lower(),
             )
         )
         results[1].append(
             PROMPT_TEMPLATE.format(
-                val_object.replace("_", " ").lower(), style.replace("_", " ").lower()
+                (
+                    style.replace("_", " ").lower()
+                    if style_arg == "name"
+                    else STYLES_TOKENS[idx]
+                ),
+                val_object.replace("_", " ").lower(),
             )
         )
         results[2].append(os.path.join(DATASET_SCP_PATH, style, train_object))
@@ -61,11 +79,11 @@ def generate_train_dataset():
     return results
 
 
-def main(style_seed, order_seed):
+def main(style_seed, order_seed, style_arg):
     path_to_save_models = get_work_dir(style_seed, order_seed)
     os.makedirs(path_to_save_models, exist_ok=True)
 
-    train_dataset = generate_train_dataset()
+    train_dataset = generate_train_dataset(style_arg)
     train_dataset = shuffle_lists(order_seed, train_dataset)
 
     serialized_lists = [
@@ -93,6 +111,13 @@ if __name__ == "__main__":
         required=True,
         help="Seed for shuffling the order of training",
     )
+    parser.add_argument(
+        "--style_arg",
+        type=str,
+        required=False,
+        help="Token to use for training",
+        choices=["token", "name"],
+    )
 
     args = parser.parse_args()
-    main(args.style_seed, args.order_seed)
+    main(args.style_seed, args.order_seed, args.style_arg)
