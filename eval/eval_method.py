5c79f18dc58176872a333c389ce1c9af78dea0f1
Lukasz Staniszewski <lukaszstaniszewski10@gmail.com>
2024-09-08 14:31:20 +0200

    feat: metrics calc

---
+++ eval/eval_method.py
@@ -0,0 +1,166 @@
+import argparse
+import json
+import os
+from pathlib import Path
+
+import numpy as np
+import torch
+from tqdm import tqdm
+
+from eval.helpers import convert_metrics_to_arrays, load_pickle, save_pickle
+from eval.metrics import calculate_cl_metrics, clip_image_metric, csd_metric, dino_metric
+from eval.plots import plot_incremental_performance_heatmap, plot_incremental_performance_plot
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+PATH_METRICS_ROOT = Path("./results/metrics/")
+OBJECT_METRICS = {
+    "csd": csd_metric,
+    "dino": dino_metric,
+}
+STYLE_METRICS = {
+    "clip": clip_image_metric,
+    "dino": dino_metric,
+}
+
+
+def convert_metrics_to_arrays(tasks_metrics_dict, n_tasks: int):
+    metrics_arrays = {}
+
+    metrics_names = tasks_metrics_dict.keys()
+    for metric_name in metrics_names:
+        square_array = np.full((n_tasks, n_tasks), np.nan)
+        for i in range(n_tasks):
+            for j in range(i + 1):
+                square_array[i, j] = tasks_metrics_dict[metric_name].get((j + 1, i + 1), np.nan)
+        metrics_arrays[metric_name] = square_array
+
+    return metrics_arrays
+
+
+def get_tasks(tasks_config_path):
+    with open(tasks_config_path, "r") as file:
+        tasks_config = json.load(file)
+    return tasks_config["tasks"]
+
+
+def find_all_orders_paths(models_path):
+    return [p for p in Path(models_path).iterdir() if p.is_dir()]
+
+
+def find_all_seeds_paths(order_path):
+    return [p for p in Path(order_path).iterdir() if p.is_dir()]
+
+
+def get_after_task_dir(order_seed_path, task_idx):
+    return (order_seed_path / f"after_task_{task_idx}").resolve()
+
+
+def get_on_task_samples(after_task_dir, task_idx):
+    return load_pickle((after_task_dir / f"on_task_{task_idx}.pkl").resolve())["samples"]
+
+
+def get_task_eval_path(task_config, task_type):
+    metrics_key = "path" if task_type == "object" else "metric_path"
+    return Path(task_config[metrics_key]).resolve()
+
+
+def calculate_metrics(model_samples, eval_path, task_type):
+    metrics_map = OBJECT_METRICS if task_type == "object" else STYLE_METRICS
+
+    return {metric_name: metrics_map[metric_name](model_samples, eval_path) for metric_name in metrics_map.keys()}
+
+
+def calc_cl_metrics(metrics_arrays, task_type, save_path):
+    metrics_map = OBJECT_METRICS if task_type == "object" else STYLE_METRICS
+
+    with open(f"{save_path}/out_cl.txt", "w") as file:
+        for metric_name in metrics_map.keys():
+            metric_vals = metrics_arrays[metric_name]
+            metric_avg_accuracy, metric_avg_forgetting = calculate_cl_metrics(metric_matrix=metric_vals)
+            output_str = f"{metric_name.upper()} | AVG_ACC={metric_avg_accuracy}\n{metric_name.upper()} AVG_FORGETTING={metric_avg_forgetting}\n"
+            file.write(output_str)
+            print(output_str)
+
+
+def average_metrics(metrics_dict, task_type):
+    metrics_map = OBJECT_METRICS if task_type == "object" else STYLE_METRICS
+
+    output_metrics = {}
+    tasks_combinations = metrics_dict[metrics_dict.keys()[0]].keys()
+    for metric_name in metrics_map.keys():
+        metric_vals = {}
+        for task_combination in tasks_combinations:
+            metric_vals[task_combination] = np.mean(
+                [metrics_dict[seed_metrics][task_combination][metric_name] for seed_metrics in metrics_dict.keys()]
+            )
+        output_metrics[metric_name] = metric_vals
+
+    return output_metrics
+
+
+def get_metrics(models_path, method_name, task_type, tasks_config):
+    tasks = get_tasks(tasks_config)
+    tasks_map = {task["index"]: task for task in tasks}
+    metrics_path = (PATH_METRICS_ROOT / task_type / method_name).resolve()
+    os.makedirs(metrics_path, exist_ok=True)
+
+    metrics_dict = {}
+    orders_paths = find_all_orders_paths(models_path)
+    for order_path in tqdm(orders_paths, desc="Orders"):
+        order_seed = order_path.name.split("order_")[1]
+        seeds_paths = find_all_seeds_paths(order_path)
+
+        for seed_path in tqdm(seeds_paths, desc="Seeds"):
+            seed = seed_path.name.split("seed_")[1]
+            seeds_results = {}
+
+            for after_task_model_idx in tqdm(tasks_map.keys(), desc="Models"):
+                after_task_dir = get_after_task_dir(seed_path, after_task_model_idx)
+
+                for task_idx in [task_idx for task_idx in tasks_map.keys() if task_idx <= after_task_model_idx]:
+                    on_task_samples = get_on_task_samples(after_task_dir, task_idx)
+                    eval_path = get_task_eval_path(tasks_map[task_idx], task_type)
+
+                    seeds_results[(after_task_model_idx, task_idx)] = calculate_metrics(
+                        model_samples=on_task_samples, eval_path=eval_path, task_type=task_type
+                    )
+
+            metrics_dict[(order_seed, seed)] = seeds_results
+
+    save_pickle(metrics_dict, Path(metrics_path, "metrics_dict.pkl"))
+    avg_metrics = average_metrics(metrics_dict, task_type)
+    save_pickle(avg_metrics, Path(metrics_path, "metrics_avg.pkl"))
+
+    metrics_arrays = convert_metrics_to_arrays(avg_metrics, len(tasks))
+    for metric_name, vals_array in metrics_arrays.items():
+        plot_incremental_performance_heatmap(
+            array=vals_array,
+            metric_name=metric_name.upper(),
+            name=task_type,
+            save_dir=metrics_path,
+        )
+        plot_incremental_performance_plot(
+            array=vals_array.T,
+            metric_name=metric_name.upper(),
+            name=task_type,
+            save_dir=metrics_path,
+        )
+        calc_cl_metrics(metrics_arrays, task_type, metrics_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--models_path", type=str, required=True)
+    parser.add_argument(
+        "--method_name", type=str, required=True, choices=["mag_max_light", "merge_and_init", "naive_cl", "ortho_init"]
+    )
+    parser.add_argument("--task_type", type=str, required=True, choices=["object", "style"])
+    parser.add_argument("--tasks_config", type=str, required=True)
+    args = parser.parse_args()
+
+    get_metrics(
+        models_path=args.models_path,
+        method_name=args.method_name,
+        task_type=args.task_type,
+        tasks_config=args.tasks_config,
+    )
--- eval/plots.py
+++ eval/plots.py
@@ -2,54 +2,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-
 def plot_incremental_performance_heatmap(
-    arrays, names, n_tasks, name="", save_dir="."
+    array, metric_name, name="", save_dir="."
 ):
-    tasks = ["<No task>"] + [f"{idx}" for idx in range(1, n_tasks + 1)]
+    tasks_x = [f"{idx}" for idx in range(1, array.shape[0]+1)]
+    tasks_y = [f"{idx}" for idx in range(1, array.shape[1]+1)]
     colormap = "YlOrRd"
 
-    # 1st array
     plt.figure(figsize=(8, 4))
     sns.heatmap(
-        arrays[0],
+        array,
         annot=True,
         fmt=".3f",
         cmap=colormap,
-        xticklabels=tasks,
-        yticklabels=tasks[1:],
+        xticklabels=tasks_x,
+        yticklabels=tasks_y,
         linewidths=0.9,
         linecolor="black",
     )
-    plt.title(f"{names[0]} Alignment Score on next tasks")
+    plt.title(f"{metric_name} Alignment on next tasks")
     plt.xlabel("Model after task")
     plt.ylabel("Performance on task")
     if name != "":
-        plt.savefig(f"{save_dir}/{name}_{names[0]}.svg")
-    plt.show()
-
-    # 2nd array
-    plt.figure(figsize=(8, 4))
-    sns.heatmap(
-        arrays[1],
-        annot=True,
-        fmt=".3f",
-        cmap=colormap,
-        xticklabels=tasks,
-        yticklabels=tasks[1:],
-        linewidths=0.9,
-        linecolor="black",
-    )
-    plt.title(f"{names[1]} Alignment Score on next tasks")
-    plt.xlabel("Model after task")
-    plt.ylabel("Performance on task")
-    if name != "":
-        plt.savefig(f"{save_dir}/{name}_{names[1]}.svg")
-    plt.show()
-
+        plt.savefig(f"{save_dir}/{name}_{metric_name}.svg")
+    else:
+        plt.show()
 
 def plot_incremental_performance_plot(
-    arrays, names, n_tasks, name="", save_dir="."
+    array, metric_name, name="", save_dir="."
 ):
     plt.style.use("ggplot")
     plt.rcParams.update(
@@ -68,57 +48,31 @@ def plot_incremental_performance_plot(
             "figure.figsize": (14, 7),
         }
     )
-    colors = plt.cm.viridis(np.linspace(0, 1, arrays[0].shape[1] + 1))
+    colors = plt.cm.viridis(np.linspace(0, 1, array.shape[1] + 1))
     markers = ["o", "s", "^", "D", "P", "X", "H"]
-    tasks = ["<No task>"] + [f"{idx}" for idx in range(1, n_tasks + 1)]
+    tasks = [f"{idx}" for idx in range(1, array.shape[0]+1)]
 
-    # array 1st
     plt.figure(figsize=(10, 4))
-    for task_finished in range(arrays[0].shape[1] + 1):
-        if task_finished == 0:
-            y = arrays[0][0, :]
-            x = np.arange(y.shape[0]) + 1
-            plt.plot(
-                x,
-                y,
-                marker=markers[task_finished],
-                color=colors[task_finished],
-                linestyle="--",
-                linewidth=2,
-                markersize=7,
-                label="Base",
-            )
-        elif task_finished == 1:
-            y = arrays[0][task_finished, 0]
-            x = [1]
-            plt.plot(
-                x,
-                y,
-                marker=markers[task_finished],
-                color=colors[task_finished],
-                linestyle="",
-                markersize=7,
-                label=f"Model after task {task_finished}",
-            )
-        else:
-            y = arrays[0][task_finished, :]
-            x = np.arange(y.shape[0]) + 1
-            plt.plot(
-                x,
-                y,
-                marker=markers[task_finished],
-                color=colors[task_finished],
-                linestyle="-",
-                linewidth=2,
-                markersize=7,
-                label=f"Model after task {task_finished}",
-            )
+    for task_finished in range(0, array.shape[1]):
+        y = array[0][task_finished, :]
+        x = np.arange(y.shape[0])
+        plt.plot(
+            x,
+            y,
+            marker=markers[task_finished],
+            color=colors[task_finished],
+            linestyle="-",
+            linewidth=2,
+            markersize=7,
+            label=f"Model after task {task_finished}",
+        )
+
     plt.title(
-        f"{names[0]} Alignment on each task\nduring each phase of task-incremental training",
+    f"{metric_name} Alignment on each task\nduring each phase of task-incremental training",
         fontsize=13,
     )
     plt.xlabel("Number of task", fontsize=14, color="gray")
-    plt.ylabel(f"{names[0]} Score", fontsize=14, color="gray")
+    plt.ylabel(f"{metric_name} Score", fontsize=14, color="gray")
     plt.xticks(
         ticks=np.arange(1, len(tasks)), labels=tasks[1:], fontsize=12, color="gray"
     )
@@ -133,69 +87,6 @@ def plot_incremental_performance_plot(
     plt.grid(True, linestyle="-", linewidth=0.7, color="white", alpha=0.7)
     plt.tight_layout(rect=[0, 0, 0.85, 1])
     if name != "":
-        plt.savefig(f"{save_dir}/{name}_{names[0]}.svg")
-    plt.show()
-
-    # 2nd array
-    plt.figure(figsize=(10, 4))
-    for task_finished in range(arrays[1].shape[1] + 1):
-        if task_finished == 0:
-            y = arrays[1][0, :]
-            x = np.arange(y.shape[0]) + 1
-            plt.plot(
-                x,
-                y,
-                marker=markers[task_finished],
-                color=colors[task_finished],
-                linestyle="--",
-                linewidth=2,
-                markersize=7,
-                label="Base",
-            )
-        elif task_finished == 1:
-            y = arrays[1][task_finished, 0]
-            x = [1]
-            plt.plot(
-                x,
-                y,
-                marker=markers[task_finished],
-                color=colors[task_finished],
-                linestyle="",
-                markersize=7,
-                label=f"Model after task {task_finished}",
-            )
-        else:
-            y = arrays[1][task_finished, :]
-            x = np.arange(y.shape[0]) + 1
-            plt.plot(
-                x,
-                y,
-                marker=markers[task_finished],
-                color=colors[task_finished],
-                linestyle="-",
-                linewidth=2,
-                markersize=7,
-                label=f"Model after task {task_finished}",
-            )
-    plt.title(
-        f"{names[1]} Image Alignment on each task\nduring each phase of task-incremental training",
-        fontsize=13,
-    )
-    plt.xlabel("Number of task", fontsize=14, color="gray")
-    plt.ylabel(f"{names[1]} Score", fontsize=14, color="gray")
-    plt.xticks(
-        ticks=np.arange(1, len(tasks)), labels=tasks[1:], fontsize=12, color="gray"
-    )
-    plt.yticks(fontsize=12, color="gray")
-    plt.legend(
-        title="Model type",
-        fontsize=10,
-        title_fontsize="12",
-        bbox_to_anchor=(1.05, 1),
-        loc="upper left",
-    )
-    plt.grid(True, linestyle="-", linewidth=0.7, color="white", alpha=0.7)
-    plt.tight_layout(rect=[0, 0, 0.85, 1])
-    if name != "":
-        plt.savefig(f"{save_dir}/{name}_{names[1]}.svg")
-    plt.show()
+        plt.savefig(f"{save_dir}/{name}_{metric_name}.svg")
+    else:
+        plt.show()
